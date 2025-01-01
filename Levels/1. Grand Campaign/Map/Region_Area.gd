0db285608cd7a8d4f2891ebbcfe3dae65a0e6109
Thomas-Holtvedt <thomas.holtvedt@gmail.com>
2025-01-01 14:40:40 +0100

    Fixed errors
    Working state

--- Levels/1. Grand Campaign/Map/Region_Area.gd
+++ Levels/1. Grand Campaign/Map/Region_Area.gd
@@ -7,48 +7,34 @@ var region_center = Vector2(0,0)
 var clicked = false
 
 func _on_child_entered_tree(node):
-	#if node.is_class("Polygon2D"):
 	if node.is_class("Line2D"):
-		#node.color = Color(1, 1, 1, 0.5)
 		node.default_color = Color(1, 1, 1, 0.5)
 		node.modulate = Color( 1, 1, 1, 0)
 	
-	
 func _on_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
+		clear_selection()
 		for node in get_children():
 			if node.is_class("Line2D"):
-			#if node.is_class("Polygon2D"):
-				#node.color = Color(1, 0, 0, 0.5)
-				clear_selection()
 				node.default_color = Color(1, 0, 0, 0.5)
 				node.modulate = Color( 1, 1, 1, 1)
 				self.clicked = true
-				
 
 func _on_mouse_entered():
 	for node in get_children():
 		if node.is_class("Line2D"):
-		#if node.is_class("Polygon2D"):
 			node.modulate = Color( 1, 1, 1, 1)
 
 func _on_mouse_exited():
 	for node in get_children():
 		if node.is_class("Line2D"):
-		#if node.is_class("Polygon2D"):
 			if self.clicked == false:
 				node.modulate = Color( 1, 1, 1, 0)
 
 func clear_selection():
 	for region in get_parent().get_children():
 		for node in region.get_children():
-			#if node.is_class("Polygon2D"):
 			if node.is_class("Line2D"):
-				#node.color = Color(1, 1, 1, 0.5)
 				node.default_color = Color(1, 1, 1, 0.5)
 				node.modulate = Color( 1, 1, 1, 0)
 				region.clicked = false
-				#node.modulate = Color( 1, 1, 1, 0)
-
-
-
